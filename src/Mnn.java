6d869c66a4b9de2dd602e129d32b3aa04d7c3f25
ankita3111 <paulchoudhury_a@yahoo.in>
2018-10-25 20:03:26 +0530

    added

--- src/Mnn.java
+++ src/Mnn.java
@@ -2,6 +2,8 @@
 public class Mnn {
 	public static void main(String[] args) {
 		System.out.println("base class");
+		System.out.println("added second classs");
+		System.out.println("one");
 	}
 
 }
